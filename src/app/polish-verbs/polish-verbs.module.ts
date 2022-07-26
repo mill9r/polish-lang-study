b0fe188b87aae4b6d5be0942ee1738caa09c7094
andrei_tsylko <mill9r@gmail.com>
2022-07-26 11:45:32 +0200

    init: create angular project and add angular-material and NgRx

---
+++ src/app/polish-verbs/polish-verbs.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { PolishVerbsRoutingModule } from './polish-verbs-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    PolishVerbsRoutingModule
+  ]
+})
+export class PolishVerbsModule { }
